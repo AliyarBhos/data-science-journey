b5330182f04eeffb1601ef5e4d0a15797999a59d
AliyarBhos <aliyar.bayramov2006@gmail.com>
2026-08-11 13:40:16 +0400

    adding Level 5 .py file with Playwright implemented

--- 01_data_collection/level5_scraper.py
+++ 01_data_collection/level5_scraper.py
@@ -1,18 +1,20 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
-def get_rendered_html(url):
+def get_rendered_html(url, wait_selector=None):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(url)
+        if wait_selector:
+            page.wait_for_selector(wait_selector, timeout=10000)
         html = page.content()
         browser.close()
         return html
 
 
 if __name__ == '__main__':
-    html = get_rendered_html('http://quotes.toscrape.com/js/')
+    html = get_rendered_html('http://quotes.toscrape.com/js/', wait_selector='span.text')
     print('Lenght of html', len(html))
 
     soup = BeautifulSoup(html, 'html.parser')
